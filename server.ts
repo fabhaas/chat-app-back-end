39a4f0baa94751079b6893af4bc5c50d8fe429aa
fahaas <fahaas@edu.aau.at>
2019-01-26 17:03:52 +0100

    removed unused test code

--- server.ts
+++ server.ts
@@ -15,7 +15,7 @@ const wss: WebSocket.Server = new WebSocket.Server({
 }, () => console.log(`WebSocket server listening on port ${wss.options.port}.`));
 
 initSockets(wss);
-app.get("/", (req, res) => { res.sendFile("/home/fahaas/Uni/Webtechnologien/PR/back-end/test/index.html"); });
+
 app.use(express.json()); //enable json bodies
 app.use((req, res, next) => {
     //for enabling cors
